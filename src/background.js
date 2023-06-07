a370b66e48e9684399915b625e392bec1cfde471
Edwin Zarco <64220994+Edwin-Zarco@users.noreply.github.com>
2023-06-07 10:21:42 +0000

    Refactor
    The code is separated into individual functions for better organization and readability.
    The context menu creation, button click event registration, and context menu click event registration are each placed in their respective functions.
    Meaningful function names are used to indicate the purpose of each function.
    The arrow function syntax is used consistently for event listeners.
    Comments are removed since the code is now self-explanatory.

--- src/background.js
+++ src/background.js
@@ -1,35 +1,32 @@
-/*
- * Hand the URL of the current tab over to the content script.
- * TODO: user feedback for 'bad' URLs
- */
 function invoke(info, tab) {
   var url = tab.url;
   if (info) {
     url = info.linkUrl || info.srcUrl;
   }
-  console.log("play-with attempting to play url " + url);
-  browser.tabs.sendMessage(tab.id, {url: url});
+  console.log("replay-with attempting to play URL: " + url);
+  browser.tabs.sendMessage(tab.id, { url: url });
 }
 
-/*
- * Create the context menu for links.
- */
-browser.contextMenus.create({
-  id: "play-with",
-  title: browser.i18n.getMessage("actionName"),
-  contexts: ["link", "video"]
-});
+function createContextMenu() {
+  browser.contextMenus.create({
+    id: "replay-with",
+    title: browser.i18n.getMessage("actionName"),
+    contexts: ["link", "video"]
+  });
+}
 
-/*
- * Register listener for the button.
- */
-browser.browserAction.onClicked.addListener((tab) => {
-  invoke(null, tab);
-});
+function registerButtonClickEvent() {
+  browser.browserAction.onClicked.addListener(tab => {
+    invoke(null, tab);
+  });
+}
 
-/*
- * Register listener for the context menu.
- */
-browser.contextMenus.onClicked.addListener((info, tab) => {
-  invoke(info, tab);
-});
+function registerContextMenuClickEvent() {
+  browser.contextMenus.onClicked.addListener((info, tab) => {
+    invoke(info, tab);
+  });
+}
+
+createContextMenu();
+registerButtonClickEvent();
+registerContextMenuClickEvent();
